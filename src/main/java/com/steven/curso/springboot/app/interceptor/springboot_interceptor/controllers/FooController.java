7074e99d9879bdd670773b11b12bd35c9591289a
parra06 <santiago.parra.0276@eam.edu.co>
2024-05-30 13:07:58 -0500

    Agregar metodo tri

--- src/main/java/com/steven/curso/springboot/app/interceptor/springboot_interceptor/controllers/FooController.java
+++ src/main/java/com/steven/curso/springboot/app/interceptor/springboot_interceptor/controllers/FooController.java
@@ -19,13 +19,19 @@ public class FooController {
 
     @GetMapping("/bar")
     public Map<String, String> bar() {
-
+        
         return Collections.singletonMap("message", "handler bar del controlador");
     }
-
     @GetMapping("/baz")
     public Map<String, String> baz() {
 
         return Collections.singletonMap("message", "handler baz del controlador");
     }
+    @GetMapping("/tri")
+    public Map<String, String> tri() {
+
+        return Collections.singletonMap("message", "handler tri del controlador");
+    }
+    
+    
 }
