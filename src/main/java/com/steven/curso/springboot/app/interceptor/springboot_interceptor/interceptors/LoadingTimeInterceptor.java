4d0ff75fc891014543631bef4049fab856ea0340
parra06 <santiago.parra.0276@eam.edu.co>
2024-05-30 12:49:28 -0500

    Initial commit

---
+++ src/main/java/com/steven/curso/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -0,0 +1,81 @@
+package com.steven.curso.springboot.app.interceptor.springboot_interceptor.interceptors;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+// import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component("timeInterceptor")
+public class LoadingTimeInterceptor implements HandlerInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoadingTimeInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+    throws Exception {
+        logger.info("LoadingTimeInterceptor: preHandle() entrando..." 
+        + ((HandlerMethod)handler).getMethod().getName());
+
+        long start = System.currentTimeMillis();
+        request.setAttribute("start", start);
+
+
+        Random random = new Random();
+        int delay = random.nextInt(500);
+        Thread.sleep(delay);
+
+        /* PERSONALIZAR PARA EL RETURN FALSE
+
+        Map<String,String> json = new HashMap<>();
+        json.put("error", "No tienes acceso a esta pagina!");
+        json.put("date", new Date().toString());
+        
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = mapper.writeValueAsString(json);
+
+        response.setContentType("application/json");
+        response.setStatus(401);
+
+        response.getWriter().write(jsonString);
+
+        return false;
+         */
+
+        return true;
+}
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            @Nullable ModelAndView modelAndView) throws Exception {
+                
+                long end = System.currentTimeMillis();
+                long start = (long) request.getAttribute("start");
+                long result = end - start;
+
+                logger.info("Tiempo trnscurrido: " + result + "milisegundos!");
+
+
+
+
+                HandlerMethod controller = ((HandlerMethod)handler);
+                logger.info("LoadingTimeInterceptor: postHandle() saliendo..."
+                 + controller.getMethod().getName()  );
+                
+            
+                }
+            
+
+}
